e0bda47f219bb135c46e6789daf8e9e02db0f06f
sgalcheung <zhangsigao@live.com>
2020-06-25 15:41:12 +0800

    initial experience

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,32 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * Page initial data
-   */
   data: {
-
+    name: 'SgalCheung',
+    age: 18,
+    students: [
+      {id: 110, name: 'kobe', age: 30},
+      {id: 111, name: 'james', age: 28},
+      {id: 112, name: 'curry', age: 32},
+      {id: 113, name: 'why', age: 18}
+    ],
+    counter: 0
   },
+  handleBtnClick() {
+    // 1.错误做法: 界面是不会刷新的
+    // this.data.counter += 1
+    // console.log(this.data.counter)
 
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad: function (options) {
-
+    // 2.this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })
+
+// 编程范式:
+// 1.命令式编程: 原生操作DOM
+// 2.声明式编程: Vue/React/Angular
