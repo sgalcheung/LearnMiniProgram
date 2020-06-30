1a62960dbe8314239fa67306e77f1787211c806a
sgalcheung <zhangsigao@live.com>
2020-06-30 20:58:30 +0800

    network request and encapsulation

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,59 @@
 // pages/home/home.js
-Page({
+import request from '../../service/network.js'
 
-  /**
-   * Page initial data
-   */
+Page({
   data: {
 
   },
-
-  /**
-   * Lifecycle function--Called when page load
-   */
   onLoad: function (options) {
+    // 1.原生的方式发送网络请求
+    this.get_data_origin()
 
+    // 2.使用封装的request发送网络请求
+    // Promise最大的好处就是防止出现回调地狱
+    request({
+      url: 'http://123.207.32.32:8000/recommend',
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+    
   },
-
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
+  get_data_origin() {
+    // 发送网络请求
+    // 1.发送最简单的get请求
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/recommend',
+    //   success: function(res) {
+    //     console.log(res)
+    //   }
+    // })
+    // 2.get请求，并且携带参数
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/home/data',
+    //   data: {
+    //     type: "sell",
+    //     page: 1
+    //   },
+    //   success: function(res) {
+    //     console.log(res)
+    //   }
+    // })
+    // 3.post请求，并且携带参数
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method: "post",
+    //   data: {
+    //     name: "sgalcheung",
+    //     age: 18
+    //   },
+    //   success: function(res) {
+    //     console.log(res)
+    //   },
+    //   fail: function(err) {
+    //     console.log(err)
+    //   }
+    // })
   }
 })
